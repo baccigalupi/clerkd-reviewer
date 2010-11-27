97c2d13f5db942b29d1140ae5cef2a9d88ad9ec1
Adolfo von Zastrow <fito@Adolfo-von-Zastrows-MacBook-Pro-15.local>
2010-11-27 14:53:28 -0800

    User::Name persists to database and parses strings into name objects

--- app/models/user/name.rb
+++ app/models/user/name.rb
@@ -1,4 +1,36 @@
 class User
   class Name
+    include MongoMapper::EmbeddedDocument
+    
+    key :first, String
+    key :last, String
+    key :middle, String
+    
+    def self.parse(string)
+      name = new
+      if string.include?(' ')
+        array = string.split(' ')
+        if string.include?(',')
+          name.first = array.last
+          name.last = array.first.chop
+        else
+          if array.size > 2 
+            name.first = array.first
+            name.middle = array.slice(1, ((array.size) - 2)).join(' ')
+            name.last = array.last
+          else  
+            name.first = array.first
+            name.last = array.last
+          end
+        end
+      else
+        array = string.split(',')
+        name.first = array.last
+        name.last = array.first
+      end
+      name
+    end
+  
   end
 end
+
